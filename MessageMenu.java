8b78cc6632d1d7970e17724536b651b804ec426c
Jean Redfern <jeanredfern@Jeans-MacBook-Pro.local>
2021-03-16 01:34:11 -0400

    Created viewMessages method

--- MessageMenu.java
+++ MessageMenu.java
@@ -1,8 +1,13 @@
 import java.util.Scanner;
+import java.io.File;
+import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.IOException; 
+import java.io.FileReader;
 
 public class MessageMenu
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         int userChoice;
         Scanner input = new Scanner(System.in);
@@ -10,7 +15,7 @@ public class MessageMenu
 
         System.out.println("Please choose from the following options: ");
         System.out.println("------------------------------------------");
-        System.out.println("1. Past Messages");
+        System.out.println("1. View Messages");
         System.out.println("2. New Message");
         System.out.println("3. Archive Message");
         System.out.println("4. Delete Message");
@@ -19,8 +24,36 @@ public class MessageMenu
 
         userChoice = input.nextInt();
         
-        
+        switch(userChoice) {
+            case 1:
+                viewMessages();
+            case 2:
+
+        }
     }
 
-    
+    public static void viewMessages() throws IOException {
+        HashMap<String, String> messages = new HashMap<String, String>();
+            String line;
+            BufferedReader messageReader = new BufferedReader(new FileReader("messages.txt"));
+            try {
+                while ((line = messageReader.readLine()) != null ) {
+                    String[] records = line.split(":", 2);
+                    if (records.length >= 2) {
+                        String messageID = records[0];
+                        String messageContent = records[1];
+                        messages.put(messageID, messageContent);
+                    } else {
+                        System.out.println("ignoring line: " + line);
+                    }
+                }
+                for (String key : messages.keySet())
+                {
+                    System.out.println(key + ":" + messages.get(key));
+                }
+                messageReader.close();
+            } catch (IOException e) {
+                System.out.println("IO Exception");
+            }
+    }
 }
